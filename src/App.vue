7eba9990c9ad61cfb5140a6989caab20286c591e
ankurraj <ankur.raj@xpressbees.com>
2022-06-13 22:56:48 +0530

    Shailvi comments

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
-import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 import Todo from './components/Todo.vue';
 </script>
 
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -2,37 +2,36 @@
 </script>
 
 <template>
-  <table class="table table-borderless">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+    <div>
+        <div>Name : {{name}}</div>
+        <div class="d-flex">
+            <input type="text" placeholder="Enter text" class="form-control" />
+            <button class="btn btn-warning rounded-0">SUBMIT</button>
+        </div>
+        <table class="table table-bordered mt-5">
+            <thead>
+                <tr>
+                    <th scope="col">Task</th>
+                    <th scope="col">Status</th>
+                    <th scope="col" class="text-center">#</th>
+                    <th scope="col" class="text-center">#</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Mark</td>
+                    <td>To-do</td>
+                    <td>
+                        <div class="text-center"><span class="fa fa-pen"></span></div>
+                    </td>
+                    <td>
+                        <div class="text-center"><span class="fa fa-trash"></span></div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <style>
-
 </style>
